50df6851576639abb56d244e16deb95d5e60595e
Ioannis Magkanaris <iomagkanaris@gmail.com>
2023-10-15 20:10:05 +0200

    Solved and added details in 02_01_functional_generic_utilities/code/apply_tuple_bug.cpp

--- 02_01_functional_generic_utilities/code/apply_tuple_bug.cpp
+++ 02_01_functional_generic_utilities/code/apply_tuple_bug.cpp
@@ -9,15 +9,20 @@ template <typename... Ts>
 std::size_t f(Ts... ts) {
     constexpr auto g = [](auto... ts) {
         // Print the types in ts...
-        // std::cout << '\n';
-        // (std::cout << ... << typeid(ts).name()) << '\n';
+        std::cout << '\n';
+        (std::cout << ... << typeid(ts).name()) << '\n';
         return sizeof...(ts); };
 
     // Are the following lines equivalent? Why? Why not?  What does class
     // template argument deduction do? See
     // https://eel.is/c++draft/over.match.class.deduct#1.3.
-    return std::invoke(g, ts...);
-    // return std::apply(g, std::tuple(ts...));
+    // In std::invoke ts.. is a tuple the second when f(std::tuple(42, 3.14, std::string("hello"))) is called and then the tuple is passed to the invoke
+    // and g is going to be called with a tuple
+    // return std::invoke(g, ts...);
+    // In std::apply ts.. is a tuple the second when f(std::tuple(42, 3.14, std::string("hello"))) is called and because of the CTAD, the copy constructor of
+    // std::tuple is called thus passing in the end an std::tuple to std::apply, which is what is expects. So the g() is going to be called with the arguments
+    // without being wrapped in a tuple.
+    return std::apply(g, std::tuple(ts...));
 }
 
 int main() {
